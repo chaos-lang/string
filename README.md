36d74b90c9fd151789aaae9dbeec2c885eaa551c
Mehmet Mert Yildiran <mehmetmertyildiran@gmail.com>
2020-04-25 17:23:55 +0300

    Add string.replace() function

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-String library of the Chaos language.
+String library of the Chaos language. You can install this spell with:
+
+```bash
+occultist install string
+```
+
+and import it with:
+
+```chaos
+import string
+```
+
+## String operations
+
+### string.replace(str target, str needle, str replacement)
+
+Replace all occurrences of the `needle` string with the `replacement` string.
+
+```chaos
+kaos> string.replace("hello world", "world", "friend")
+hello friend
+```
--- string.c
+++ string.c
@@ -1,19 +1,78 @@
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+
 #include "Chaos.h"
 
-char *hello_params_name[] = {};
-unsigned hello_params_type[] = {};
-unsigned short hello_params_length = 0;
-int KAOS_EXPORT Kaos_hello()
+// string.replace(str haystack, str needle, str replacement)
+
+char *str_replace(char *haystack, char *needle, char *replacement) {
+    char *result;
+    char *ins;
+    char *tmp;
+    int len_rep;
+    int len_with;
+    int len_front;
+    int count;
+
+    if (!haystack || !needle)
+        return NULL;
+    len_rep = strlen(needle);
+    if (len_rep == 0)
+        return NULL;
+    if (!replacement)
+        replacement = "";
+    len_with = strlen(replacement);
+
+    ins = haystack;
+    for (count = 0; (tmp = strstr(ins, needle)); ++count) {
+        ins = tmp + len_rep;
+    }
+
+    tmp = result = malloc(strlen(haystack) + (len_with - len_rep) * count + 1);
+
+    if (!result)
+        return NULL;
+
+    while (count--) {
+        ins = strstr(haystack, needle);
+        len_front = ins - haystack;
+        tmp = strncpy(tmp, haystack, len_front) + len_front;
+        tmp = strcpy(tmp, replacement) + len_with;
+        haystack += len_front + len_rep; // move to next "end of rep"
+    }
+    strcpy(tmp, haystack);
+    return result;
+}
+
+char *replace_params_name[] = {
+    "target",
+    "needle",
+    "replacement"
+};
+unsigned replace_params_type[] = {
+    K_STRING,
+    K_STRING,
+    K_STRING
+};
+unsigned short replace_params_length = (unsigned short) sizeof(replace_params_type) / sizeof(unsigned);
+int KAOS_EXPORT Kaos_replace()
 {
-    fprintf(stdout, "Hello from the string!\n");
-    fflush(stdout);
+    char* target = kaos.getVariableString(replace_params_name[0]);
+    char* needle = kaos.getVariableString(replace_params_name[1]);
+    char* replacement = kaos.getVariableString(replace_params_name[2]);
+    char* result = str_replace(target, needle, replacement);
+    free(target);
+    free(needle);
+    free(replacement);
+    kaos.returnVariableString(result);
     return 0;
 }
 
 int KAOS_EXPORT KaosRegister(struct Kaos _kaos)
 {
     kaos = _kaos;
-    kaos.defineFunction("hello", K_VOID, hello_params_name, hello_params_type, hello_params_length);
+    kaos.defineFunction("replace", K_STRING, replace_params_name, replace_params_type, replace_params_length);
 
     return 0;
 }
